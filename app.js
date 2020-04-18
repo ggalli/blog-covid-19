c0f458525754d002685fa2748a81be03bd0732b3
Guilherme <guilhermegalli5@gmail.com>
2020-04-18 13:33:10 -0300

    refactor fetchData to show loading

--- app.js
+++ app.js
@@ -1,31 +1,36 @@
 (function () {
+    const container = document.querySelector('.container');
+    const loading = document.querySelector('.loading');
+
     function ajaxNavigation(hash) {
         if (!hash) return
 
         const link = document.querySelector(`[href='${hash}']`);
-
-        // link.classList.add('active');
         if (!link) return
 
         let url = hash.substring(1);
-        if(url == '/') url = '/news.html';
+        if (url == '/') url = '/news.html';
 
         fetchData(url);
-    }   
-
-    function fetchData(url) {
-        const destino = document.querySelector('.container');
-
-        fetch(url)
-            .then(resp => resp.text())
-            .then(html => {
-                destino.innerHTML = html;
-            })
     }
 
+    function fetchData(url) {
+        container.innerHTML = '';
+        loading.style.display = 'block';
+
+        setTimeout(() => {
+            fetch(url)
+                .then(resp => resp.text())
+                .then(html => {
+                    container.innerHTML = html;
+                })
+
+            loading.style.display = 'none';
+        }, 2000);
+
+
+    }
 
     window.onload = e => fetchData('/news.html')
-
     window.onhashchange = e => ajaxNavigation(location.hash)
-    
 })()
